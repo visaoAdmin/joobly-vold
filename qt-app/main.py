ac7ae572833e1f6701c8225a8034a21655621e18
Aakash Yadav <aakash@mywoobly.com>
2021-09-21 08:54:15 +0530

    ADD: waiterlite prototype screens

--- qt-app/main.py
+++ qt-app/main.py
@@ -5,6 +5,8 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QDialog
 import os 
 
+isWaiterCalled = False
+
 def yellowLight():
     os.system("sudo python3 /home/pi/scripts/neopixel-yellow.py")
 
@@ -102,19 +104,171 @@ class TapForServiceScreen(QDialog):
     def __init__(self):
         super(TapForServiceScreen, self).__init__()
         loadUi("ui/10TapForServiceScreen.ui", self)
+        yellowLight()
         self.goToNextButton.clicked.connect(self.navigateToCloseServiceScreen)
+        self.menuButton.clicked.connect(self.navigateToDinerActionMenu)
     
     def navigateToCloseServiceScreen(self):
         navigateToScreen(CloseServiceScreen)
+    
+    def navigateToDinerActionMenu(self):
+        navigateToScreen(DinerActionMenuScreen)
 
 class CloseServiceScreen(QDialog):
     def __init__(self):
         super(CloseServiceScreen, self).__init__()
         loadUi("ui/11CloseServiceScreen.ui", self)
-    #     self.goToNextButton.clicked.connect(self.navigateToCloseServiceScreen)
+        global isWaiterCalled
+        isWaiterCalled = True
+        blueLight()
+        self.goToNextButton.clicked.connect(self.navigateToTapForServiceScreen)
+        self.menuButton.clicked.connect(self.navigateToDinerActionMenu)
     
-    # def navigateToCloseServiceScreen(self):
-    #     navigateToScreen(CloseServiceScreen)
+    def navigateToTapForServiceScreen(self):
+        global isWaiterCalled
+        isWaiterCalled = False
+        navigateToScreen(TapForServiceScreen)
+    
+    def navigateToDinerActionMenu(self):
+        navigateToScreen(DinerActionMenuScreen)
+
+class DinerActionMenuScreen(QDialog):
+    def __init__(self):
+        super(DinerActionMenuScreen, self).__init__()
+        loadUi("ui/12DinerActionMenuScreen.ui", self)
+        self.goToNextButton.clicked.connect(self.navigateToQuickMenuScreen)
+        self.backButton.clicked.connect(self.navigateBack)
+    
+    def navigateToQuickMenuScreen(self):
+        navigateToScreen(QuickMenuScreen)
+    
+    def navigateBack(self):
+        global isWaiterCalled
+        if isWaiterCalled:
+            navigateToScreen(CloseServiceScreen)
+        else:
+            navigateToScreen(TapForServiceScreen)
+
+class QuickMenuScreen(QDialog):
+    def __init__(self):
+        super(QuickMenuScreen, self).__init__()
+        loadUi("ui/13QuickMenuScreen.ui", self)
+        self.drinksButton.clicked.connect(self.navigateToDrinkItemScreen)
+        self.mainCourseButton.clicked.connect(self.navigateToFoodMenuScreen)
+        self.startersButton.clicked.connect(self.navigateToFoodMenuScreen)
+        self.chefSpecialsButton.clicked.connect(self.navigateToFoodMenuScreen)
+        self.dessertsButton.clicked.connect(self.navigateToFoodMenuScreen)
+        self.backButton.clicked.connect(self.navigateBack)
+    
+    def navigateBack(self):
+        navigateToScreen(DinerActionMenuScreen)
+
+    def navigateToDrinkItemScreen(self):
+        navigateToScreen(DrinkItemScreen)
+    
+    def navigateToFoodMenuScreen(self):
+        navigateToScreen(FoodMenuScreen)
+
+class DrinkItemScreen(QDialog):
+    def __init__(self):
+        super(DrinkItemScreen, self).__init__()
+        loadUi("ui/14DrinkItemScreen.ui", self)
+        # self.goToNextButton.clicked.connect(self.navigateToDrinkItemScreen)
+        self.backButton.clicked.connect(self.navigateBack)
+    
+    def navigateBack(self):
+        navigateToScreen(DinerActionMenuScreen)
+
+# class FoodMenuScreen(QDialog):
+#     def __init__(self):
+#         super(FoodMenuScreen, self).__init__()
+#         loadUi("ui/15FoodMenuScreen.ui", self)
+#         # self.goToNextButton.clicked.connect(self.navigateToDrinkItemScreen)
+#         self.backButton.clicked.connect(self.navigateBack)
+    
+#     def navigateBack(self):
+#         navigateToScreen(DinerActionMenuScreen)
+
+class FoodMenuScreen(QDialog):
+    def __init__(self):
+        super(FoodMenuScreen, self).__init__()
+        loadUi("ui/15FoodMenuScreen.ui", self)
+        self.backButton.clicked.connect(self.navigateBack)
+        self.cartButton.clicked.connect(self.navigateToCartScreen)
+        self.goToNextButton.clicked.connect(self.navigateToCartScreen)
+    
+    def navigateBack(self):
+        navigateToScreen(QuickMenuScreen)
+
+    def navigateToCartScreen(self):
+        navigateToScreen(CartScreen)
+
+class CartScreen(QDialog):
+    def __init__(self):
+        super(CartScreen, self).__init__()
+        loadUi("ui/16CartScreen.ui", self)
+        self.backButton.clicked.connect(self.navigateBack)
+        self.goToNextButton.clicked.connect(self.navigateToBillScreen)
+    
+    def navigateBack(self):
+        navigateToScreen(FoodMenuScreen)
+
+    def navigateToBillScreen(self):
+        navigateToScreen(BillScreen)
+
+class BillScreen(QDialog):
+    def __init__(self):
+        super(BillScreen, self).__init__()
+        loadUi("ui/17BillScreen.ui", self)
+        self.backButton.clicked.connect(self.navigateBack)
+        self.payButton.clicked.connect(self.navigateToPayScreen)
+        self.feedbackButton.clicked.connect(self.navigateToFeedbackScreen)
+    
+    def navigateBack(self):
+        navigateToScreen(DinerActionMenuScreen)
+
+    def navigateToPayScreen(self):
+        navigateToScreen(PayQRScreen)
+    
+    def navigateToFeedbackScreen(self):
+        navigateToScreen(FeedbackScreen)
+
+class PayQRScreen(QDialog):
+    def __init__(self):
+        super(PayQRScreen, self).__init__()
+        loadUi("ui/18PayQRScreen.ui", self)
+        self.backButton.clicked.connect(self.navigateBack)
+        self.goToNextButton.clicked.connect(self.navigateToFeedbackScreen)
+    
+    def navigateBack(self):
+        navigateToScreen(DinerActionMenuScreen)
+    
+    def navigateToFeedbackScreen(self):
+        navigateToScreen(FeedbackScreen)
+
+class FeedbackScreen(QDialog):
+    def __init__(self):
+        super(FeedbackScreen, self).__init__()
+        loadUi("ui/19FeedbackScreen.ui", self)
+        self.backButton.clicked.connect(self.navigateBack)
+        self.goToNextButton.clicked.connect(self.navigateToThankYouScreen)
+    
+    def navigateBack(self):
+        navigateToScreen(PayQRScreen)
+    
+    def navigateToThankYouScreen(self):
+        navigateToScreen(ThankYouScreen)
+
+class ThankYouScreen(QDialog):
+    def __init__(self):
+        super(ThankYouScreen, self).__init__()
+        loadUi("ui/20ThankYouScreen.ui", self)
+        self.goToNextButton.clicked.connect(self.navigateToIdleLockScreen)
+
+    
+    def navigateToIdleLockScreen(self):
+        navigateToScreen(IdleLockScreen)
+
 
 
 
