52f129fe2ad0574ef2ac85934fccfa3289cd22f1
Aman Gupta <aman.gupta@squareboat.com>
2023-01-11 16:21:20 +0530

    FIX: start.sh hard reset

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/pi/waiterlite-raspberry && 
-git restore . &&
+git reset --hard &&
 git pull origin &
 ./startup-scripts.sh &
 cd /home/pi/waiterlite-raspberry/qt-app && /usr/bin/python3 main.py
