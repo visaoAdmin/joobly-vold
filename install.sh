d7e1f59eb1cab1bf94617cb5b30ca99917c4bf9f
Aman Gupta <aman.gupta@squareboat.com>
2023-01-16 12:15:07 +0530

    ADD: REDIS INSTALL SCRIPT

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
-sudo apt install redis-server && sudo systemctl start redis && sudo systemctl start redis && wget http://download.redis.io/redis-stable.tar.gz -P ~/ &&
-cd ~/redis-stable/
-&&
-make -j$(nproc)
-&&
-sudo make install
+#!/bin/bash
+sudo apt install redis-server && 
+sudo systemctl start redis && 
+sudo systemctl start redis && 
+wget http://download.redis.io/redis-stable.tar.gz -P ~/ &&
+tar xvf ~/redis-stable.tar.gz -C ~/ &&
+cd ~/redis-stable/ &&
+make -j$(nproc) &&
+sudo make install &&
+pip3 install rq
