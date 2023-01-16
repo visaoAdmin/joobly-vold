b9a0d03c77123b5793290f95a0eeed322cb01928
Aman Gupta <aman.gupta@squareboat.com>
2023-01-16 12:32:45 +0530

    ADD: REDIS INSTALL SCRIPT

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 cd /home/pi/waiterlite-raspberry && 
 git pull origin &
 ./startup-scripts.sh &
-cd /home/pi/waiterlite-raspberry/qt-app && sh start_worker.sh && /usr/bin/python3 main.py
+cd /home/pi/waiterlite-raspberry/qt-app && export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES && 
+rq worker high --with-scheduler & /usr/bin/python3 main.py
