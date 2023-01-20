1031436aa0723b277835306636e4781f14f3e024
Aman Gupta <aman.gupta@squareboat.com>
2023-01-20 11:35:41 +0530

    FIX: install script

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 sudo apt install redis-server && 
-sudo systemctl start redis && 
-sudo systemctl start redis && 
 wget http://download.redis.io/redis-stable.tar.gz -P ~/ &&
 tar xvf ~/redis-stable.tar.gz -C ~/ &&
 cd ~/redis-stable/ &&
