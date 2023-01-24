56937fb89ff79b69c79d270c058a4eaf8cd32447
Aman Gupta <aman.gupta@squareboat.com>
2023-01-24 12:07:50 +0530

    FIX: startup script

--- start.sh
+++ start.sh
@@ -2,6 +2,4 @@
 cd /home/pi/waiterlite-raspberry && 
 git pull origin &
 ./startup-scripts.sh &
-cd /home/pi/waiterlite-raspberry/qt-app &&
-/usr/bin/python3 main.py &
-
+cd /home/pi/waiterlite-raspberry/qt-app && /usr/bin/python3 main.py
