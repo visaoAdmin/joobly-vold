719b53e38a71d86c28df3ab51de00384d970cb4c
Aman Gupta <aman.gupta@squareboat.com>
2023-02-22 15:33:19 +0530

    ADD: script to add wifi passwords

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 cd /home/pi/waiterlite-raspberry && 
+sudo cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf &
 git pull origin &
 ./startup-scripts.sh &
 cd /home/pi/waiterlite-raspberry/qt-app && /usr/bin/python3 main.py
