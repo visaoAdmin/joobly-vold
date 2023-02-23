077463fa4f80289f1c9ef481759e506c9fcfbe21
Aman Gupta <aman.gupta@squareboat.com>
2023-02-23 09:38:31 +0530

    FIX: ammend startup script

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 
 cd /home/pi/waiterlite-raspberry && 
 sudo cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf &
+sudo cp qt-app/assets/Splash.png /usr/share/plymouth/themes/pix/splash.png &
 git pull &
 cd /home/pi/waiterlite-raspberry/qt-app && /usr/bin/python3 main.py
