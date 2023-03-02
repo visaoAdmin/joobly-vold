a4d0c708f7c17a0ab978c2168e074ce18a5bb2e4
Aman Gupta <aman.gupta@squareboat.com>
2023-03-02 11:52:15 +0530

    FIX: repair script

--- repair.sh
+++ repair.sh
@@ -4,5 +4,5 @@ if grep -e "main.py" /home/pi/waiterlite-raspberry/start.sh
 then
         echo "Done"
 else
-        cd /home/pi && sudo rm -rf waiterlite-raspberry && git clone --branch develop https://aakash-woobly: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@github.com/Radien-$
+        cd /home/pi && sudo rm -rf waiterlite-raspberry && git clone --branch develop https://aakash-woobly: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@github.com/Radien-Design/waiterlite-raspberry.git
 fi
