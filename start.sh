dd0f256f8f360f31c6de4a8145482b1b27c5d27e
Aman Gupta <aman.gupta@squareboat.com>
2023-03-03 17:34:31 +0530

    FIX: remove copy repar

--- start.sh
+++ start.sh
@@ -7,5 +7,4 @@ sudo cp qt-app/assets/Splash.png /usr/share/plymouth/themes/pix/splash.png &
 sudo cp desktop-items-0.conf /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf &
 sudo cp waiterLITE.desktop /etc/xdg/autostart/waiterLITE.desktop  &
 sudo cp waiterLITERepair.desktop /etc/xdg/autostart/waiterLITERepair.desktop  &
-cp repair.sh /home/pi/repair.sh &
 cd /home/pi/waiterlite-raspberry/qt-app && /usr/bin/python3 main.py
