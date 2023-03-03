e28645c5a26e68c27af56014d8b9919f63b6fdbd
Aman Gupta <aman.gupta@squareboat.com>
2023-03-03 12:33:59 +0530

    FIX: repair.sh

--- repair.sh
+++ repair.sh
@@ -5,7 +5,7 @@ while ! iwgetid | grep -e "SSID" ; do
         sleep 1
 done
 
-if cd /home/pi/waiterlite-raspberry && git pull | grep -e "fatal" 
+if cd /home/pi/waiterlite-raspberry && git checkout -f develop && git pull origin develop | grep -e "error:" 
 then
         cd /home/pi && sudo rm -rf waiterlite-raspberry && git clone --branch develop https://aakash-woobly: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@github.com/Radien-Design/waiterlite-raspberry.git
 else
