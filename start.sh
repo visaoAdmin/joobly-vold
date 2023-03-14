241b8dfa8e50ba8b8d4fc0304d8add6a9516f169
Aman Gupta <aman.gupta@squareboat.com>
2023-03-14 14:42:22 +0530

    FIX: remove auto set wifi password

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@
 
 
 cd /home/pi/waiterlite-raspberry && 
-sudo cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf &
 sudo cp qt-app/assets/Splash.png /usr/share/plymouth/themes/pix/splash.png &
 sudo cp desktop-items-0.conf /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf &
 sudo cp waiterLITE.desktop /etc/xdg/autostart/waiterLITE.desktop  &
