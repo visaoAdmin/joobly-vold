15e87f57084f90f116360bf8c86fc9472cdf664f
Aman Gupta <aman.gupta@squareboat.com>
2023-03-31 18:42:52 +0530

    FIX: remove repair script

--- start.sh
+++ start.sh
@@ -5,5 +5,4 @@ cd /home/pi/waiterlite-raspberry &&
 sudo cp qt-app/assets/Splash.png /usr/share/plymouth/themes/pix/splash.png &
 sudo cp desktop-items-0.conf /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf &
 sudo cp waiterLITE.desktop /etc/xdg/autostart/waiterLITE.desktop  &
-sudo cp waiterLITERepair.desktop /etc/xdg/autostart/waiterLITERepair.desktop  &
 cd /home/pi/waiterlite-raspberry/qt-app && /usr/bin/python3 main.py &> /home/pi/lastLaunchLog.txt
