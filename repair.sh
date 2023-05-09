2ceb157eb625fd057d44728307b2f43f7071b0d8
Aman Gupta <aman.gupta@squareboat.com>
2023-05-09 10:27:09 +0530

    gitignore changes

--- repair.sh
+++ repair.sh
@@ -5,9 +5,9 @@ while ! iwgetid | grep -e "SSID" ; do
         sleep 1
 done
 
-if cd /home/pi/waiterlite-raspberry && git checkout -f develop && git pull origin develop | grep -e "error:" 
+if cd /home/pi/waiterlite-raspberry && git checkout -f release && git pull origin release | grep -e "error:" 
 then
-        cd /home/pi && sudo rm -rf waiterlite-raspberry && git clone --branch develop https://aakash-woobly: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@github.com/Radien-Design/waiterlite-raspberry.git
+        cd /home/pi && sudo rm -rf waiterlite-raspberry && git clone --branch release https://aakash-woobly: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@github.com/Radien-Design/waiterlite-raspberry.git
 else
         echo "Done 1"
 fi
@@ -16,5 +16,5 @@ if grep -e "main.py" /home/pi/waiterlite-raspberry/start.sh
 then
         echo "Done2"
 else
-        cd /home/pi && sudo rm -rf waiterlite-raspberry && git clone --branch develop https://aakash-woobly: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@github.com/Radien-Design/waiterlite-raspberry.git
+        cd /home/pi && sudo rm -rf waiterlite-raspberry && git clone --branch release https://aakash-woobly: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@github.com/Radien-Design/waiterlite-raspberry.git
 fi
